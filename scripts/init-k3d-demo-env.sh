2c31afb5bbd765323fa07ba29aa130136e866e97
Jakub Slatinsky <slatinskyj@gmail.com>
2023-04-21 15:05:26 +0200

    create customer2

--- scripts/init-k3d-demo-env.sh
+++ scripts/init-k3d-demo-env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 MYDIR=$(dirname $0)
-kind create cluster
+#kind create cluster
 helm repo add argo https://argoproj.github.io/argo-helm && helm repo update
 # https://github.com/cf-kubecon23-demo/cf-kubecon23-demo/issues/2121
 helm install --repo https://argoproj.github.io/argo-helm --create-namespace --namespace argocd argocd argo-cd --version 5.21.0  --set "configs.cm.application\.resourceTrackingMethod=annotation" --wait
